63555f979931e35a4e30c34a0c13df54fa4e9269
JDudas <jdudas12@gmail.com>
2025-02-17 09:56:22 -0500

    newapp

---
+++ songApp/src/components/App.tsx
@@ -0,0 +1,13 @@
+import Button from 'react-bootstrap/Button';
+
+
+function App()  {
+    return  (
+        <div>
+        
+        <Button>Submit</Button>
+        </div>
+    );
+}
+
+export default App;
--- songApp/src/components/main.tsx
+++ songApp/src/components/main.tsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App_old.tsx'
-import "bootstrap/dist/css/bootstrap.min.css"
-import { Card, CardBody } from "react-bootstrap"
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
+
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
